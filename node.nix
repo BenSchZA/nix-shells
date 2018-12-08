59d89a36ca09aff71130ee63b0d70c657c1ae8ef
Benjamin Scholtz <ben@linumlabs.com>
2018-12-08 22:38:04 +0200

    Update config

--- node.nix
+++ node.nix
@@ -8,5 +8,8 @@ stdenv.mkDerivation {
     shellHook = ''
 	alias yarn="yarn --ignore-engines"
         export PATH="$PWD/node_modules/.bin/:$PATH"
+	npm config set cache-min 9999999
+	npm config set registry http://localhost:8081/repository/npm-proxy
+	yarn config set registry http://localhost:8081/repository/npm-proxy
     '';
 }
---
+++ rust-wasm.nix
@@ -0,0 +1,22 @@
+# See https://gist.github.com/LightDiscord/aa8cb8e67ec9efbf9b20a9a3e9ebd007
+
+#$ # Then you can run any cargo-web command. (You need to specify the target)
+#$ cargo-web build --target=wasm32-unknown-unknown
+#$ cargo-web start --target=wasm32-unknown-unknown
+#$ cargo-web deploy --target=wasm32-unknown-unknown
+
+with import <nixpkgs> {
+  overlays = map (uri: import (fetchTarball uri)) [
+    https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz
+  ];
+};
+
+stdenv.mkDerivation {
+  name = "rust-wasm";
+  buildInputs = [
+    cargo-web
+    (latest.rustChannels.nightly.rust.override {
+      targets = ["wasm32-unknown-unknown"];
+    })
+  ];
+}
