22ac6880c2d1d3234b2bbbff656d6ff1fd7ccd76
Benjamin Scholtz <contact@benjaminscholtz.com>
2020-02-20 11:10:12 +0200

    Update profiling shell

--- profiling.nix
+++ profiling.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> { };
 
 stdenv.mkDerivation {
   name = "profiling";
-  buildInputs = [ valgrind hotspot linuxPackages.perf perf-tools valkyrie ];
+  buildInputs = [ bench valgrind hotspot linuxPackages.perf perf-tools valkyrie ];
   shellHook = ''
     echo "valgrind --tool=massif --pages-as-heap=yes --massif-out-file=massif.out _"
   '';
